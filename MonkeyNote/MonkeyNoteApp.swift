5e52fe475e8eb098af8d837fad39c1ecafd44099
Cavaldos <khanhnguyen297kn@gmail.com>
2025-12-25 11:15:01 +0700

    Initial Commit

---
+++ MonkeyNote/MonkeyNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonkeyNoteApp.swift
+//  MonkeyNote
+//
+//  Created by Nguyen Ngoc Khanh on 25/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MonkeyNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
