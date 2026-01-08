61095ed057b6225fa3d55cdee53ffea5ca38e6b8
Cavaldos <khanhnguyen297kn@gmail.com>
2026-01-08 15:44:58 +0700

    release - version 1.3.4

--- MonkeyNote/AppConfig.swift
+++ MonkeyNote/AppConfig.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 struct AppConfig {
-    static let version = "1.3.3"
+    static let version = "1.3.4"
     static let buildNumber = 1
     
     static let appName = "MonkeyNote"
